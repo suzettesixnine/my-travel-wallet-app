015856fc00d6dbcbdda2ab4db8760a0582b5b6ab
Suzette <suzettesixnine@gmail.com>
2025-07-07 09:02:52 -0700

    Final attempt to fix syntax errors and deploy

--- src/App.js
+++ src/App.js
@@ -355,7 +355,7 @@ const App = () => {
                             className="flex-grow px-6 py-3 bg-slate-800 text-white font-semibold rounded-full shadow-lg hover:bg-slate-900 transition duration-300 ease-in-out transform hover:-translate-y-1"
                             disabled={viewingShared}
                         >
-                            <svg className="w-5 h-5 inline-block mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.684 13.342C8.882 12.682 9 12 9 12s.118-.682.316-1.342m0 2.684a3 3 0 100-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 0a3 3 0 105.367 2.684 3 3 0 00-5.367-2.684z"></path></svg>
+                            <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.684 13.342C8.882 12.682 9 12 9 12s.118-.682.316-1.342m0 2.684a3 3 0 100-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 0a3 3 0 105.367 2.684 3 3 0 00-5.367-2.684z"></path></svg>
                             Publish My Current Itinerary
                         </button>
                         <button
